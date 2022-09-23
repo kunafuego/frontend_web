d0cfaadf53114b3bcbb0c587dddcc1e7b16e5d43
PabloSanFrancisco <psanfrancisco@uc.cl>
2022-09-23 17:36:40 -0300

    Base simulador

--- README.md
+++ README.md
@@ -13,19 +13,19 @@
 
 ##### 3. La posibilidad de navegar entre distintas páginas HTML (usen la etiqueta de anchor para ello) ✅
 
-##### 4. Una página de inicio de su aplicación WEB, que sea “amigable” es decir, de navegación obvia, con títulos, menús, sub páginas y secciones que orienten al usuario.
+##### 4. Una página de inicio de su aplicación WEB, que sea “amigable” es decir, de navegación obvia, con títulos, menús, sub páginas y secciones que orienten al usuario. ✅
 
 ##### 5. Las consideraciones, guía y reglas detalladas para el uso de su aplicación, a nivel de usuario como de administrador (al menos esos niveles se han de considerar).
 
 ##### 6. Una sección “acerca de..”, muchas veces conocida como “about...” donde explican qué es esto!, la versión, etc.
 
-##### 7. Una sección de “nosotros” donde se identifique el grupo (nombre) y se individualice a sus integrantes.
+##### 7. Una sección de “nosotros” donde se identifique el grupo (nombre) y se individualice a sus integrantes. ✅
 
-##### 8. De ser necesario, deberán entregar también, en el directorio correspondiente, todas las imágenes y otros recursos requeridos para el despliegue correcto de su sitio.
+##### 8. De ser necesario, deberán entregar también, en el directorio correspondiente, todas las imágenes y otros recursos requeridos para el despliegue correcto de su sitio. ✅
 
 ##### 9. Una barra de navegación que estará presente siempre en su página (en la parte superior) que permitirá ir a “home” o a cualquiera de las secciones que ustedes entreguen (recomendación, una sección debiese corresponder a una página HTML). ✅
 
-##### 10.Deberán entregar Javascript de lado cliente (archivos .js) que permitan hacer algunas acciones básicas sobre sus páginas y que sean sólo de carácter local, por ejemplo usar elementos HTML como un tag selection y que ante la selección de una opción cambie algo en la página, o el uso de botones que realicen acciones locales, etc. Recuerden que esto es parte integral de vuestro diseño de front.
+##### 10.Deberán entregar Javascript de lado cliente (archivos .js) que permitan hacer algunas acciones básicas sobre sus páginas y que sean sólo de carácter local, por ejemplo usar elementos HTML como un tag selection y que ante la selección de una opción cambie algo en la página, o el uso de botones que realicen acciones locales, etc. Recuerden que esto es parte integral de vuestro diseño de front. ✅
 
 #### C: Todo lo descrito en el enunciado como mínimo (listado de 10 puntos), pero con un desarrollo menos elaborado (Landing page, about us, about the Game, instrucciones, etc.) 
 
--- src/index.js
+++ src/index.js
@@ -16,3 +16,25 @@ function tipo_input(valor){
     }
 }
 
+function tipo_producto(valor){
+    if (valor == "mueble"){
+        document.getElementById("idparrafo").innerHTML = "Estos muebles utilizan un sistema de pegado ultravioleta invisible, líquido e incoloro. Se puede pegar vidrio con vidrio o vidrio con acero. Su principal característica es proyectar transparencia y claridad al interior del lugar incorporando la más alta calidad y tecnología en el proceso de pegado de cristales."
+        document.getElementById("idfoto").src = "../assets/imgs/mueble.jpeg"
+    } else if (valor == "lyf") {
+        document.getElementById("idparrafo").innerHTML = "La cristalería de laboratorio se refiere a una variedad de equipos utilizados en el trabajo científico y tradicionalmente hechos de vidrio. El vidrio se puede soplar, doblar, cortar, moldear y formar en muchos tamaños y formas y, por lo tanto, es común en los laboratorios de química, biología y análisis."
+        document.getElementById("idfoto").src = "../assets/imgs/laboratorio.jpeg"
+    } else if (valor == "espejo") {
+        document.getElementById("idparrafo").innerHTML = "Los espejos modernos consisten en una delgada capa de plata o aluminio depositado sobre una plancha de vidrio, la cual protege el metal y hace al espejo más duradero. Este proceso se conoce como plateado."
+        document.getElementById("idfoto").src = "../assets/imgs/espejo.jpeg"
+    } else if (valor == "reciclado") {
+        document.getElementById("idparrafo").innerHTML = "Tiene una gran cantidad de usos y aplicaciones. Para la fabricación de mosaicos y azulejos, para encimeras de cuartos de baño y cocinas, para la fabricación de todo tipo de envases, y para fachadas e interiores."
+        document.getElementById("idfoto").src = "../assets/imgs/reciclado.jpeg"
+    } else if (valor == "seguridad") {
+        document.getElementById("idparrafo").innerHTML = "Un vidrio de seguridad es aquel que en caso de rotura no presenta potencial para causar heridas de consideración a las personas, un concepto adicional y complementario al de la seguridad es el concepto de protección que, en general, está ligado con las propiedades de aquellos vidrios difíciles de ser traspasados por el impacto de personas u objetos."
+        document.getElementById("idfoto").src = "../assets/imgs/seguridad.jpeg"
+    } else {
+        document.getElementById("idparrafo").innerHTML = "Bienvenido al simulador de SOA, para poder utilizarlo debe elegir un producto de la lista de productos, rellenar con la cantidad de unidades que desea producir y finalmente el rango de fechas que le gustaría dar a su proyecto"
+        document.getElementById("idfoto").src = "../assets/imgs/fotoprueba.png"
+    }
+}
+
